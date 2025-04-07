6259c6957e50007d465474f8ef20dc4275203e76
unknown <thangkay3009@gmail.com>
2025-04-07 15:51:10 +0700

    first commit

---
+++ app/src/main/java/com/example/hybridmusicapp/ui/splash/fragment/SplashFragment2.kt
@@ -0,0 +1,11 @@
+package com.example.hybridmusicapp.ui.splash.fragment
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class SplashFragment2 : Fragment() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
