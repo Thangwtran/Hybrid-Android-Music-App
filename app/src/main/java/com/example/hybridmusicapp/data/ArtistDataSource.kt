cf3a4832d770c973f7ad60c56d0662212dd59640
Thang Tran <thangkay3009@gmail.com>
2025-04-18 10:05:31 +0700

    artist local data source base

--- app/src/main/java/com/example/hybridmusicapp/data/ArtistDataSource.kt
+++ app/src/main/java/com/example/hybridmusicapp/data/ArtistDataSource.kt
@@ -1,7 +1,15 @@
 package com.example.hybridmusicapp.data
 
+import com.example.hybridmusicapp.data.model.artist.Artist
+import kotlinx.coroutines.flow.Flow
+
 interface ArtistDataSource {
-    interface Local{}
+    interface Local{
+        fun getArtists(): Flow<List<Artist>>
+        suspend fun insertArtist(vararg artist: Artist)
+        suspend fun deleteArtist(artist: Artist)
+        suspend fun updateArtist(artist: Artist)
+    }
 
     interface Remote{}
 
--- app/src/main/java/com/example/hybridmusicapp/data/source/local/artist/LocalArtistDataSource.kt
+++ app/src/main/java/com/example/hybridmusicapp/data/source/local/artist/LocalArtistDataSource.kt
@@ -1,9 +1,26 @@
 package com.example.hybridmusicapp.data.source.local.artist
 
 import com.example.hybridmusicapp.data.ArtistDataSource
+import com.example.hybridmusicapp.data.model.artist.Artist
+import kotlinx.coroutines.flow.Flow
 
 class LocalArtistDataSource (
     private val artistDao: ArtistDao
 ): ArtistDataSource.Local{
+    override fun getArtists(): Flow<List<Artist>> {
+        return artistDao.artists
+    }
+
+    override suspend fun insertArtist(vararg artist: Artist) {
+        artistDao.insertArtist(*artist)
+    }
+
+    override suspend fun deleteArtist(artist: Artist) {
+        artistDao.deleteArtist(artist)
+    }
+
+    override suspend fun updateArtist(artist: Artist) {
+        artistDao.updateArtist(artist)
+    }
 
 }
