e2bcad511c76ac500a89020ddfacf8ca0e88fad6
Thang Tran <thangkay3009@gmail.com>
2025-04-18 10:14:28 +0700

    recent songs local data source base

--- app/src/main/java/com/example/hybridmusicapp/data/RecentDataSource.kt
+++ app/src/main/java/com/example/hybridmusicapp/data/RecentDataSource.kt
@@ -1,5 +1,11 @@
 package com.example.hybridmusicapp.data
 
+import com.example.hybridmusicapp.data.model.recent.RecentSong
+import com.example.hybridmusicapp.data.model.song.Song
+import kotlinx.coroutines.flow.Flow
+
 // Local
 interface RecentDataSource {
+    fun getRecentSongs(): Flow<List<Song>>
+    suspend fun insert(vararg songs: RecentSong)
 }
--- app/src/main/java/com/example/hybridmusicapp/data/source/local/recent_song/RecentSongDataSource.kt
+++ app/src/main/java/com/example/hybridmusicapp/data/source/local/recent_song/RecentSongDataSource.kt
@@ -1,4 +1,19 @@
 package com.example.hybridmusicapp.data.source.local.recent_song
 
-class RecentSongDataSource {
+import com.example.hybridmusicapp.data.RecentDataSource
+import com.example.hybridmusicapp.data.model.recent.RecentSong
+import com.example.hybridmusicapp.data.model.song.Song
+import kotlinx.coroutines.flow.Flow
+
+class RecentSongDataSource(
+    private val recentSongDao: RecentSongDao
+) : RecentDataSource {
+    override fun getRecentSongs(): Flow<List<Song>> {
+        return recentSongDao.recentSongs
+    }
+
+    override suspend fun insert(vararg songs: RecentSong) {
+        return recentSongDao.insert(*songs)
+    }
+
 }
