1cd66b228cead4d236c1c53bdb202be5a223abd3
Thang Tran <thangkay3009@gmail.com>
2025-04-21 13:13:08 +0700

    add network and playlist dialog

---
+++ app/src/main/java/com/example/hybridmusicapp/ui/dialog/NetworkDialogFragment.kt
@@ -0,0 +1,9 @@
+package com.example.hybridmusicapp.ui.dialog
+
+import androidx.fragment.app.DialogFragment
+
+class NetworkDialogFragment(
+    private val titleId: Int,
+    private val messageId: Int
+) : DialogFragment() {
+}
